14ac2c9133d9d68f3aaa9faa94dd5aa9d7976ccc
Berchon <berchon@gmail.com>
2026-02-27 20:41:42 -0300

    feat(i18n): add pt and en catalogs for h6.1 t6.1.2

---
+++ src/i18n/i18n.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+
+if [[ -z "${i18n_module_loaded:-}" ]]; then
+  i18n_module_loaded=1
+
+  readonly I18N_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+  # shellcheck source=src/i18n/keys.sh
+  source "${I18N_DIR}/keys.sh"
+  # shellcheck source=src/i18n/locale_pt.sh
+  source "${I18N_DIR}/locale_pt.sh"
+  # shellcheck source=src/i18n/locale_en.sh
+  source "${I18N_DIR}/locale_en.sh"
+
+  declare -g i18n_current_language="en"
+  declare -Ag i18n_catalog=()
+fi
+
+i18n_normalize_language() {
+  local language="${1:-}"
+  language="${language,,}"
+
+  case "${language}" in
+    pt|en)
+      printf '%s\n' "${language}"
+      ;;
+    *)
+      printf 'en\n'
+      ;;
+  esac
+}
+
+i18n_load_catalog() {
+  local language="${1:-en}"
+  language="$(i18n_normalize_language "${language}")"
+
+  i18n_locale_pt_load
+  i18n_locale_en_load
+
+  i18n_catalog=()
+  i18n_current_language="${language}"
+
+  case "${language}" in
+    pt)
+      i18n_catalog=()
+      for key in "${!I18N_LOCALE_PT[@]}"; do
+        i18n_catalog["${key}"]="${I18N_LOCALE_PT[${key}]}"
+      done
+      ;;
+    en)
+      i18n_catalog=()
+      for key in "${!I18N_LOCALE_EN[@]}"; do
+        i18n_catalog["${key}"]="${I18N_LOCALE_EN[${key}]}"
+      done
+      ;;
+  esac
+}
+
+i18n_translate() {
+  local key="$1"
+
+  [[ -v "i18n_catalog[${key}]" ]] || return 1
+  printf '%s\n' "${i18n_catalog[${key}]}"
+}
+
+i18n_translatef() {
+  local key="$1"
+  shift
+  local template=""
+
+  template="$(i18n_translate "${key}")" || return 1
+  printf "${template}\n" "$@"
+}
